92045352b0a4972716a0b3a1d6c024b5889a643f
Zach Allaun <zach.allaun@gmail.com>
2023-10-17 08:51:59 -0400

    Don't do any version manager setup in shell scripts

--- apps/remote_control/priv/port_wrapper.sh
+++ apps/remote_control/priv/port_wrapper.sh
@@ -1,27 +1,7 @@
 #!/usr/bin/env bash
 
-detect_version_manager() {
-    if command -v asdf >/dev/null && asdf which elixir >/dev/null 2>&1; then
-        echo "asdf"
-    elif command -v rtx >/dev/null && rtx which elixir >/dev/null 2>&1; then
-        echo "rtx"
-    else
-        echo "not_detected"
-    fi
-}
-
 # Start the program in the background
-case "$(detect_version_manager)" in
-    asdf)
-        asdf env erl exec "$@" &
-        ;;
-    rtx)
-        eval "$(rtx env -s bash)"
-        ;;
-    *)
-        exec "$@" &
-        ;;
-esac
+exec "$@" &
 pid1=$!
 
 # Silence warnings from here on
--- bin/start_lexical.sh
+++ bin/start_lexical.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-detect_version_manager() {
-    if command -v asdf >/dev/null && asdf which elixir >/dev/null 2>&1; then
-        echo "asdf"
-    elif command -v rtx >/dev/null && rtx which elixir >/dev/null 2>&1; then
-        echo "rtx"
-    else
-        echo "not_detected"
-    fi
-}
-
-# Start the program in the background
-case "$(detect_version_manager)" in
-    asdf)
-        asdf env erl exec "$@" &
-        ;;
-    rtx)
-        eval "$(rtx env -s bash)"
-        ;;
-    *) ;;
-esac
-
 case $1 in
     iex)
         ELIXIR_COMMAND=iex
